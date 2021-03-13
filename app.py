6c24ee593a14c97a6c26382548a454187abaadee
Rashmi <rashmi.manandhar7@gmail.com>
2021-03-13 15:11:21 -0500

    feat: list all the artists from db

--- app.py
+++ app.py
@@ -290,17 +290,15 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
-  # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  data = []
+  artists = Artist.query.with_entities(Artist.id,Artist.name).all();
+  for artist in artists:
+    artist_info = {
+      'id': artist.id,
+      'name': artist.name
+    }
+    data.append(artist_info)
+ 
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
